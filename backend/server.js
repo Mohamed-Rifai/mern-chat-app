0b4d5d022a83e634774e8ccc47f3df66fb8d2954
Mohamed Rifai <rifaeeckm@gmail.com>
2023-02-18 00:09:00 +0530

    user signup

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/user')
+ require('colors');
+connectDB()
+
 const app = express()
 
+app.use(express.json());
+// app.use(express.urlencoded({ extended:true}))
+
+
 app.get('/',(req,res)=>{
-    res.send('api runnign')
+    res.send('api running')
 })
  
-app.get('/api/chats',(req,res)=>{
-    res.send('alpoliiiii')
-})
+app.use('/api/user',userRoutes)
 
 const port = process.env.PORT
 
-app.listen(port,console.log(`server started on ${port}`))
+app.listen(port,console.log(`server started on ${port}`.yellow.bold))
